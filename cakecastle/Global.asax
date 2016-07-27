964d34f2c344a58b7524dccc569c5a4a3c3f6004
Mansi <mpatel.y@gmail.com>
2016-07-26 22:37:09 -0400

    Add project files.

---
+++ cakecastle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cakecastle.MvcApplication" Language="C#" %>
---
+++ cakecastle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cakecastle.Startup))]
+namespace cakecastle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
